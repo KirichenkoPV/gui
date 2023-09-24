59e2cc8149183d5aff6d764cfaa1432667b48e66
Secatrillo <106627730+Secatrillo@users.noreply.github.com>
2023-09-24 21:10:29 +0300

    ha

--- build/external/glfw/cmake_uninstall.cmake
+++ build/external/glfw/cmake_uninstall.cmake
@@ -1,9 +1,9 @@
 
-if (NOT EXISTS "D:/gui/build/external/glfw/install_manifest.txt")
-    message(FATAL_ERROR "Cannot find install manifest: \"D:/gui/build/external/glfw/install_manifest.txt\"")
+if (NOT EXISTS "C:/Users/galat/OneDrive/gui/build/external/glfw/install_manifest.txt")
+    message(FATAL_ERROR "Cannot find install manifest: \"C:/Users/galat/OneDrive/gui/build/external/glfw/install_manifest.txt\"")
 endif()
 
-file(READ "D:/gui/build/external/glfw/install_manifest.txt" files)
+file(READ "C:/Users/galat/OneDrive/gui/build/external/glfw/install_manifest.txt" files)
 string(REGEX REPLACE "\n" ";" files "${files}")
 
 foreach (file ${files})
